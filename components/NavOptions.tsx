5b3f7b9090b651fc2c265144e48d94335ff80d7f
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 19:56:14 +0100

    ✨ NavOptions component added

---
+++ components/NavOptions.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements'
+
+type NavProps = {
+   id: string;
+   title: string;
+   image: string;
+   screen: string;
+}
+
+const data = [
+   {
+      id: "123",
+      title: "Get a ride",
+      image: "https://links.papareact.com/3pn",
+      screen: "MapScreen"
+   },
+   {
+      id: "456",
+      title: "Order food",
+      image: "https://links.papareact.com/28w",
+      screen: "EatsScreen"
+   }
+];
+
+const NavOptions = () => {
+   return (
+      <FlatList
+         keyExtractor={(item) => item.id}
+         data={data}
+         horizontal
+         renderItem={({ item }: { item: NavProps }) => (
+            <TouchableOpacity
+               style={
+                  [tw`p-2 pb-8 pt-4 pl-3 bg-gray-200 m-2 w-40`]
+               }>
+               <View>
+                  <Image
+                     source={{
+                        uri: item.image
+                     }}
+                     style={{
+                        width: 120,
+                        height: 120,
+                        resizeMode: 'contain'
+                     }}
+                  />
+                  <View style={tw`mt-2 flex-row items-center`}>
+                     <Text style={tw`text-lg font-semibold mr-1`}>{item.title}</Text>
+                     <Icon
+                        style={tw`bg-black rounded-full w-10 p-2 ml-1`}
+                        name='arrowright'
+                        type='antdesign'
+                        color='#fff'
+                     />
+                  </View>
+               </View>
+            </TouchableOpacity>
+         )}
+      />
+   )
+}
+
+export default NavOptions;
+
