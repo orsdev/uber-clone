11c957f6295e670a2e6f2c627727b3f0d9c85937
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 19:56:38 +0100

    🔥 unused Text import removed

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 import NavOptions from '../components/NavOptions';
