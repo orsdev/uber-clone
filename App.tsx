09876cf7fdd81063f0257b48c8d625796c9fecb6
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 16:38:41 +0100

    ✨ redux config set up in app.ts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 
