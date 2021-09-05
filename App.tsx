4dc272800cdd10273682452de57a5e0f9a5565e2
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 19:56:58 +0100

    ✨ react-native-element config setup in app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'react-native-elements';
+
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
+      <ThemeProvider>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </ThemeProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
