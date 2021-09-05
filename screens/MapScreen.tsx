b63e6f73e9168d0ee9f55c850fdf36a7988d329e
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 20:48:53 +0100

    ✨ MapScreen added

---
+++ screens/MapScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MapScreen = () => {
+   return (
+      <View>
+         <Text>Map Screen</Text>
+      </View>
+   )
+}
+
+export default MapScreen
+
