ccc38fa19846c465d23b17bde3a7abad88a505b5
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 18:39:37 +0100

    ✨ HomeScreen added

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+import NavOptions from '../components/NavOptions';
+
+const HomeScreen = () => {
+
+   return (
+      <SafeAreaView style={tw`bg-white h-full w-full`}>
+         <View style={tw`p-5 w-full`}>
+            <Image
+               source={{
+                  uri: "https://links.papareact.com/gzs"
+               }}
+               style={{
+                  width: 70,
+                  height: 70,
+                  resizeMode: 'contain'
+               }}
+            />
+
+            <NavOptions />
+         </View>
+      </SafeAreaView>
+   )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default HomeScreen;
