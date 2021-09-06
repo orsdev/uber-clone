7416a4c64616b0cf9fe400cf26d12385a31616ae
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 12:11:20 +0100

    ✨ map & marker added to map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { useSelector } from 'react-redux'
+
+import { RootState } from '../redux/store';
+import { OriginProps } from '../redux/slices/navSlice';
 
 const Map = () => {
+   const origin: OriginProps | null = useSelector((state: RootState) => state.nav.origin);
+
+   const getInitialRegion = () => {
+
+      if (!origin) {
+         return {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+         }
+      } else {
+         return {
+            latitude: origin?.location?.lat,
+            longitude: origin?.location?.lng,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+         }
+      }
+   }
+
    return (
       <MapView
          style={{
             flex: 1
          }}
-         initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-         }}
-      />
+         mapType="mutedStandard"
+         initialRegion={getInitialRegion()}
+      >
+         {origin?.location && (
+            <Marker
+               key={origin.description}
+               coordinate={{
+                  latitude: origin.location.lat,
+                  longitude: origin.location.lng,
+               }}
+               title="Origin"
+               identifier="origin"
+               description={origin.description}
+            />
+         )}
+      </MapView>
    )
 }
 
