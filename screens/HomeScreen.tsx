b9906dea8f8c97a06f56b60bb237b61a52b5f833
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 11:07:14 +0100

    💄 homescreen style modified

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { StyleSheet, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import GooglePlacesInput from '../components/GooglePlaceInput';
 
 import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
 
    return (
-      <SafeAreaView style={tw`bg-white h-full w-full`}>
+      <SafeAreaView style={tw`bg-white h-full w-full py-4`}>
          <View style={tw`p-5 w-full`}>
+            <View style={tw`mb-12 mt-4 overflow-hidden`}>
+               <GooglePlacesInput />
+            </View>
             <Image
                source={{
                   uri: "https://links.papareact.com/gzs"
