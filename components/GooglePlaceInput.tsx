21ce85dc63372f0acd4220ab51fa1f9dd8e9c763
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 08:13:11 +0100

    ✨ GooglePlaceInput component added

---
+++ components/GooglePlaceInput.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useDispatch } from 'react-redux'
+
+import { GOOGLE_API_KEY } from '@env';
+import { setDestination, setOrigin } from '../redux/slices/navSlice';
+
+const GooglePlacesInput = () => {
+   const dispatch = useDispatch();
+
+   return (
+      <GooglePlacesAutocomplete
+         placeholder='Where From?'
+         debounce={400}
+         nearbyPlacesAPI="GooglePlacesSearch"
+         onPress={(data, details = null) => {
+            const geometry = details?.geometry;
+            const location = geometry?.location;
+
+            dispatch(setOrigin({
+               location
+            }));
+
+         }}
+         query={{
+            key: GOOGLE_API_KEY,
+            language: 'en',
+         }}
+         minLength={2}
+         fetchDetails={true}
+         enablePoweredByContainer={false}
+         styles={{
+            container: {
+               flex: 0,
+            },
+            textInput: {
+               height: 45,
+               color: '#5d5d5d',
+               fontSize: 18,
+               width: '100%',
+               borderColor: '#ccc',
+               borderWidth: 3
+            },
+         }}
+      />
+   );
+};
+
+export default GooglePlacesInput;
