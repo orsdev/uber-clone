1d0748255131f227cf8668e811f207749b354587
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 11:07:36 +0100

    ✨ Map component used in MapScreen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+import Map from '../components/Map';
 
 const MapScreen = () => {
    return (
       <View>
-         <Text>Map Screen</Text>
+         <View style={tw`h-1/2 w-full border-4 border-red-400`}>
+            <Map />
+         </View>
+         <View style={tw`h-1/2 w-full border-2 border-green-400`}>
+            <Text style={{
+               fontSize: 20,
+               padding: 30
+            }}>Map Screen</Text>
+         </View>
       </View>
    )
 }
