047950cae0d1e03c44fab3e2771fd894cf6b8fe0
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-07 06:09:55 +0100

    ✨ MapScreen modified

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,20 +1,44 @@
 import React from 'react'
 import { Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Map from '../components/Map';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionCard from '../components/RideOptionCard';
+
+const Stack = createNativeStackNavigator();
+
+function CardStackNavigation() {
+   return (
+      <Stack.Navigator>
+         <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+               headerShown: false
+            }}
+         />
+         <Stack.Screen
+            name="RideOptionCard"
+            component={RideOptionCard}
+            options={{
+               headerShown: false
+            }}
+         />
+      </Stack.Navigator>
+   );
+}
 
 const MapScreen = () => {
    return (
       <View>
-         <View style={tw`h-1/2 w-full border-4 border-red-400`}>
+         <View style={tw`h-1/2 w-full`}>
             <Map />
          </View>
-         <View style={tw`h-1/2 w-full border-2 border-green-400`}>
-            <Text style={{
-               fontSize: 20,
-               padding: 30
-            }}>Map Screen</Text>
+         <View style={tw`h-1/2 w-full`}>
+            <CardStackNavigation />
          </View>
       </View>
    )
