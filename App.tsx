6013790e25561ff196d4754bc38ea7cc070f837f
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-08 07:18:34 +0100

    🐛 fix bug when keyboard is opened

--- App.tsx
+++ App.tsx
@@ -5,13 +5,20 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'react-native-elements';
 
 import Navigation from './navigation/Navigation';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
       <ThemeProvider>
         <SafeAreaProvider>
-          <Navigation />
+          <KeyboardAvoidingView
+            // keyboardVerticalOffset={80}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={{ flex: 1 }}
+          >
+            <Navigation />
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </ThemeProvider>
     </Provider>
